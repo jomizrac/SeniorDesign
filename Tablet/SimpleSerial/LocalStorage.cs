c5d3e060c0574fae4b7de9c8000f2e9dc6e3fce9
bblynch2 <bblynch2@ncsu.edu>
2016-09-14 19:51:52 -0400

    Added video syncing

--- Tablet/SimpleSerial/LocalStorage.cs
+++ Tablet/SimpleSerial/LocalStorage.cs
@@ -1,4 +1,5 @@
 ﻿using Amazon.S3;
+using Amazon.S3.Model;
 using Amazon.S3.Transfer;
 using System;
 using System.Collections.Generic;
@@ -23,49 +24,42 @@ namespace SimpleSerial {
 
 		#endregion Singleton
 
-		private static string existingBucketName = "*** Provide bucket name ***";
+		private static string bucketName = "*** Provide bucket name ***";
 		private static string keyName = "*** Provide your object key ***";
 		private static string filePath = "*** Provide file name ***";
 
-		public void SyncVideos() {
-			// Download all missing videos (detect if files have diff modified date?)
-			// Delete unused local videos
+		private static AmazonS3Client client;
+		private static TransferUtility fileTransferUtility;
+
+		private static string tmpDir = "C:\tmp";
+
+		private static string fileExtension = ".wmv";
+
+		public LocalStorage() {
+			client = new AmazonS3Client( Amazon.RegionEndpoint.USEast1 );
+			fileTransferUtility = new TransferUtility( client );
 		}
 
-		private static void Main( string[] args ) {
-			try {
-				TransferUtility fileTransferUtility = new TransferUtility( new AmazonS3Client( Amazon.RegionEndpoint.USEast1 ) );
-
-				// 1. Upload a file, file name is used as the object key name.
-				fileTransferUtility.Upload( filePath, existingBucketName );
-				Console.WriteLine( "Upload 1 completed" );
-
-				// 2. Specify object key name explicitly.
-				fileTransferUtility.Upload( filePath, existingBucketName, keyName );
-				Console.WriteLine( "Upload 2 completed" );
-
-				// 3. Upload data from a type of System.IO.Stream.
-				using ( FileStream fileToUpload = new FileStream( filePath, FileMode.Open, FileAccess.Read ) ) {
-					fileTransferUtility.Upload( fileToUpload, existingBucketName, keyName );
+		public void SyncVideos() {
+			// Delete any videos for products that are no longer present
+			if ( Directory.Exists( tmpDir ) ) {
+				string[] fileEntries = Directory.GetFiles( tmpDir );
+				foreach ( string filePath in fileEntries ) {
+					string fileName = Path.GetFileNameWithoutExtension( filePath );
+					bool contains = ShelfInventory.Instance.products.Any( p => p.productID.ToString() == fileName );
+					if ( !contains ) {
+						File.Delete( fileName );
+					}
 				}
-				Console.WriteLine( "Upload 3 completed" );
-
-				// 4.Specify advanced settings/options.
-				TransferUtilityUploadRequest fileTransferUtilityRequest = new TransferUtilityUploadRequest {
-					BucketName = existingBucketName,
-					FilePath = filePath,
-					StorageClass = S3StorageClass.ReducedRedundancy,
-					PartSize = 6291456, // 6 MB.
-					Key = keyName,
-					CannedACL = S3CannedACL.PublicRead
-				};
-				fileTransferUtilityRequest.Metadata.Add( "param1", "Value1" );
-				fileTransferUtilityRequest.Metadata.Add( "param2", "Value2" );
-				fileTransferUtility.Upload( fileTransferUtilityRequest );
-				Console.WriteLine( "Upload 4 completed" );
 			}
-			catch ( AmazonS3Exception s3Exception ) {
-				Console.WriteLine( s3Exception.Message, s3Exception.InnerException );
+
+			// Download any missing videos for the current product lineup
+			foreach ( var product in ShelfInventory.Instance.products ) {
+				string filePath = tmpDir + product + fileExtension;
+				if ( !File.Exists( filePath ) ) {
+					string key = product + fileExtension;
+					fileTransferUtility.Download( filePath, bucketName, key );
+				}
 			}
 		}
 	}
