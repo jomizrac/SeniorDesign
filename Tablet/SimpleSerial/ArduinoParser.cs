891ef7c8aa2a302ae896d7a50d976129babbff86
bblynch2 <bblynch2@ncsu.edu>
2016-09-14 10:32:54 -0400

    Added event sample

--- Tablet/SimpleSerial/ArduinoParser.cs
+++ Tablet/SimpleSerial/ArduinoParser.cs
@@ -4,6 +4,25 @@ using System.Linq;
 using System.Text;
 
 namespace SimpleSerial {
-	class ArduinoParser {
+
+	internal class ArduinoParser {
+
+		public delegate void ProductPickedUp( int productID );
+
+		public event ProductPickedUp ProductPickedUpEvent;
+
+		private static ArduinoParser m_instance;
+
+		public static ArduinoParser Instance {
+			get { return m_instance ?? ( m_instance = new ArduinoParser() ); }
+		}
+
+		private void SampleUsage() {
+			ArduinoParser.Instance.ProductPickedUpEvent += OnPickup;
+		}
+
+		private void OnPickup( int productID ) {
+			// Do something with productID
+		}
 	}
 }
