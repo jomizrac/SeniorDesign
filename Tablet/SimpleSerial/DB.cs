43b4b75ca1a13081d9679a89a44560f95da8acaa
bblynch2 <bblynch2@ncsu.edu>
2016-09-14 10:18:12 -0400

    Added base, empty files

---
+++ Tablet/SimpleSerial/DB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleSerial {
+
+	internal class DB {
+		private static DB m_instance;
+
+		public static DB Instance {
+			get { return m_instance ?? ( m_instance = new DB() ); }
+		}
+	}
+}
--- Tablet/SimpleSerial/Form1.cs
+++ Tablet/SimpleSerial/Form1.cs
@@ -3,67 +3,58 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Windows.Forms;
-using System.IO;
 
-namespace SimpleSerial
-{
-    public partial class Form1 : Form
-    {
-        // Add this variable 
-        string RxString;
+namespace SimpleSerial {
 
-        public Form1()
-        {
-            InitializeComponent();
-            serialPort1.PortName = "COM4";
-            serialPort1.BaudRate = 9600;
-            // serialPort1
-            serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+	public partial class Form1 : Form {
 
-            serialPort1.Open();
-            
-        }
+		// Add this variable
+		private string RxString;
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            if (serialPort1.IsOpen) serialPort1.Close();
-        }
+		public Form1() {
+			InitializeComponent();
+			serialPort1.PortName = "COM4";
+			serialPort1.BaudRate = 9600;
+			// serialPort1
+			serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler( this.serialPort1_DataReceived );
 
-        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            e.Handled = true;
-        }
+			serialPort1.Open();
+		}
 
-        private void DisplayText(object sender, EventArgs e)
-        {
-            string path = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), @"data\");
-            string file = path + RxString.Substring(0,1) + ".wmv";
-            //MessageBox.Show(file);
-            
-            TimeSpan start = new TimeSpan(9, 0, 0); //9am
-            TimeSpan end = new TimeSpan(18, 0, 0); //5pm
-            TimeSpan now = DateTime.Now.TimeOfDay;
+		private void Form1_FormClosing( object sender, FormClosingEventArgs e ) {
+			if ( serialPort1.IsOpen ) serialPort1.Close();
+		}
 
-            if ((now > start) && (now < end))
-            {
-                axWindowsMediaPlayer1.Ctlcontrols.pause();
-                axWindowsMediaPlayer1.URL = file;    //match found
-            }
-            
-            // textBox1.AppendText(RxString);
-        }
+		private void textBox1_KeyPress( object sender, KeyPressEventArgs e ) {
+			e.Handled = true;
+		}
 
-        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
-        {
-            RxString = serialPort1.ReadExisting();
-            this.Invoke(new EventHandler(DisplayText));
-        }
+		private void DisplayText( object sender, EventArgs e ) {
+			string path = Path.Combine( Path.GetDirectoryName( System.Reflection.Assembly.GetExecutingAssembly().Location ), @"data\" );
+			string file = path + RxString.Substring( 0, 1 ) + ".wmv";
+			//MessageBox.Show(file);
 
-        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
-        {
+			TimeSpan start = new TimeSpan( 9, 0, 0 ); // 9 am
+			TimeSpan end = new TimeSpan( 18, 0, 0 ); // 5 pm
+			TimeSpan now = DateTime.Now.TimeOfDay;
 
-        }
-    }
+			if ( ( now > start ) && ( now < end ) ) {
+				axWindowsMediaPlayer1.Ctlcontrols.pause();
+				axWindowsMediaPlayer1.URL = file; // match found
+			}
+
+			// textBox1.AppendText(RxString);
+		}
+
+		private void serialPort1_DataReceived( object sender, System.IO.Ports.SerialDataReceivedEventArgs e ) {
+			RxString = serialPort1.ReadExisting();
+			this.Invoke( new EventHandler( DisplayText ) );
+		}
+
+		private void axWindowsMediaPlayer1_Enter( object sender, EventArgs e ) {
+		}
+	}
 }
