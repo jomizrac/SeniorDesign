8398cf23a91088b897db815023e3250affabe013
Jonathan <Jonathan!@192.168.2.2>
2016-09-14 20:17:18 -0400

    added state and name to product class

--- Tablet/SimpleSerial/Product.cs
+++ Tablet/SimpleSerial/Product.cs
@@ -5,7 +5,10 @@ using System.Text;
 
 namespace SimpleSerial {
 
+    
 	internal class Product {
-		public int productID { get; private set; }
-	}
+        public string name { get; private set;  }
+		public int productID { get; private set; } //The ID of the product (duh)
+        public bool state { get; private set; } //false means product is in display, true means it is picked up
+    }
 }
