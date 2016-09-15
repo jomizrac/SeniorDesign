b90427563cf6e7b2f076244216fd0d1730283071
eesnider <eesnider@ncsu.edu>
2016-09-14 21:13:00 -0400

    done stuff

--- Tablet/SimpleSerial/ShelfInventory.cs
+++ Tablet/SimpleSerial/ShelfInventory.cs
@@ -3,24 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace SimpleSerial {
+namespace SimpleSerial
+{
 
-	internal class ShelfInventory {
+    internal class ShelfInventory
+    {
 
-		#region Singleton
+        #region Singleton
 
-		private static ShelfInventory m_instance;
+        private static ShelfInventory m_instance;
 
-		public static ShelfInventory Instance {
-			get { return m_instance ?? ( m_instance = new ShelfInventory() ); }
-		}
+        public static ShelfInventory Instance
+        {
+            get { return m_instance ?? (m_instance = new ShelfInventory()); }
+        }
 
-		#endregion Singleton
+        #endregion Singleton
+        //private Dictionary<int, bool> dictionary = new Dictionary<int, bool>();
+        public List<Product> products = new List<Product>();
+        private List<Product> playables = new List<Product>();
 
-		public List<Product> products = new List<Product>();
+        public void UpdateSlot(int slotNumber, Product newProduct)
+        {
+            products[slotNumber] = newProduct;
+            //store locally somehow
+        }
 
-		public void UpdateSlot( int slotNumber, Product newProduct ) {
-			products[slotNumber] = newProduct;
-		}
-	}
+        private static void Main()
+        {
+            ArduinoParser.Instance.ProductPickedUpEvent += Instance.OnProductPickup;
+            ArduinoParser.Instance.ProductPutBackEvent += Instance.OnProductPutBack;
+
+        }
+
+        private void OnProductPickup(int slotID)
+        {
+            Product current = new Product();
+            current = products[slotID]
+            current.state = true;
+            playables.Add(current);
+
+        }
+
+        private void OnProductPutBack(int ID)
+        {
+            Product current = new Product();
+            current = products[slotID]
+            current.state = true;
+            playables.Remove(current);
+        }
+    }
 }
