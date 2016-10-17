390978f46bf761235fed24cb533db7c0d6f944a0
Jonathan <Jonathan!@192.168.2.2>
2016-10-17 10:32:32 -0400

    started a logger class

---
+++ Tablet/SimpleSerial/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Logger
+{
+
+    #region Singleton
+
+    private static Logger m_instance;
+
+    public static Logger Instance
+    {
+        get { return m_instance ?? (m_instance = new Logger()); }
+    }
+
+    #endregion Singleton
+
+
+    public Logger()
+	{
+        new Thread(() => Initialize()).Start();
+    }
+    private void Initialize()
+    {
+        ShelfInventory.Instance.ProductPickUpEvent -= Instance.OnProductPickup;
+        ShelfInventory.Instance.ProductPickUpEvent += Instance.OnProductPickup;
+
+        ShelfInventory.Instance.ProductPutDownEvent -= Instance.OnProductPutDown;
+        ShelfInventory.Instance.ProductPutDownEvent += Instance.OnProductPutDown;
+
+    }
+    private void OnProductPickup(Product product)
+    {
+        DB.Instance.CreateProductItem(product, "Pick Up");
+    }
+    private void OnProductPutDown()
+    {
+        DB.Instance.CreateProductItem(product, "Put Down");
+    }
+}
