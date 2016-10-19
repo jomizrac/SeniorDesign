00198dc8658612a1a38cd11e03e99f008cf60ac5
bcfightm <bcfightm@ncsu.edu>
2016-10-19 18:06:38 -0400

    created idledetector

---
+++ Tablet/SimpleSerial/IdleDetector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleSerial
+{
+   internal class IdleDetector
+    {
+        #region Singleton
+
+        private static ArduinoParser m_instance;
+
+        public static ArduinoParser Instance
+        {
+            get { return m_instance ?? (m_instance = new ArduinoParser()); }
+        }
+
+        #endregion Singleton
+
+        #region Events
+
+        public delegate void SlotPickUp(int slotIdx);
+
+        public delegate void SlotPutDown(int slotIdx);
+
+        public event SlotPickUp SlotPickUpEvent;
+
+        public event SlotPutDown SlotPutDownEvent;
+
+        #endregion Events
+    }
+}
