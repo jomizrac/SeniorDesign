e9884f13559748fb3a070a417902a312e2a6572b
bblynch2 <bblynch2@ncsu.edu>
2016-10-20 20:57:06 -0400

    Added == and != operator overriding for Product

--- Tablet/SimpleSerial/Product.cs
+++ Tablet/SimpleSerial/Product.cs
@@ -21,6 +21,25 @@ namespace SimpleSerial {
 			this.status = status;
 		}
 
+		public static bool operator ==( Product a, Product b ) {
+			// If both are null, or both are same instance, return true.
+			if ( ReferenceEquals( a, b ) ) {
+				return true;
+			}
+
+			// If one is null, but not both, return false.
+			if ( a == null || b == null ) {
+				return false;
+			}
+
+			// Return true if the fields match:
+			return a.Equals( b );
+		}
+
+		public static bool operator !=( Product a, Product b ) {
+			return !( a == b );
+		}
+
 		public bool Equals( Product other ) {
 			if ( other == null ) return false;
 			return productID.Equals( other.productID );
