bc892d3910b3866045b7a7f77dfad03c4ba3096b
Jonathan <Jonathan!@192.168.2.2>
2016-10-24 09:28:35 -0400

    fixed product ID in DB

--- Tablet/SimpleSerial/DB.cs
+++ Tablet/SimpleSerial/DB.cs
@@ -116,7 +116,7 @@ namespace SimpleSerial
                 select nic.GetPhysicalAddress().ToString()
             ).FirstOrDefault();
             var deviceName = Environment.MachineName;
-            product["ProductID"] = currentProduct.productID + DateTime.Now.ToString();
+            product["ProductID"] = currentProduct.productID;
             product["ProductName"] = currentProduct.name;
             product["ProductLocation"] = currentProduct.slotID;
             product["ShelfMAC"] = currentShelfMAC;
