45217816252580592f5950a9d799ce596b652d65
Jonathan <Jonathan!@192.168.2.2>
2016-11-24 11:51:26 -0500

    changed some text in GUI

--- Tablet/SimpleSerial/GUI/NewProd.Designer.cs
+++ Tablet/SimpleSerial/GUI/NewProd.Designer.cs
@@ -77,8 +77,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(300, 101);
             this.label1.TabIndex = 0;
-            this.label1.Text = "You have selected \"Product ID\".  If this is correct, enter the new products UPC c" +
-    "ode.";
+            this.label1.Text = " ";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
