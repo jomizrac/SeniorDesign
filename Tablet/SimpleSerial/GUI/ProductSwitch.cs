1bc1bead43df69f36ce9acf318ce4f94f3b462dd
bcfightm <bcfightm@ncsu.edu>
2016-11-28 11:41:25 -0500

    creating new behavior for changing products

---
+++ Tablet/SimpleSerial/GUI/ProductSwitch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimpleSerial.GUI
+{
+    public partial class ProductSwitch : Form
+    {
+        public ProductSwitch()
+        {
+            InitializeComponent();
+        }
+
+        private void ProductSwitch_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void currentList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
